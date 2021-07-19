511adf0c3838c2cd6a63d228827d552e9d101a61
zhangSir12 <84233658+zhangSir12@users.noreply.github.com>
2021-07-19 09:52:17 +0800

    test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("Hello Git3");
         System.out.println("Hello Git4");
         System.out.println("hot-fix commit");
+        System.out.println("pull test");
     }
 }
